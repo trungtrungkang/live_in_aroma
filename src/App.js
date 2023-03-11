b69265f7fa59be10afff04cd129fad553621eb90
trungtrungkang <trungtrungkang@gmail.com>
2023-03-11 21:43:31 +0700

    Correct footer layout

--- src/App.js
+++ src/App.js
@@ -15,25 +15,25 @@ function App() {
         </div>
         <div>
           <img src={lp3} alt="Live In Aroma p3" />
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', 'justifyContent': 'center', fontWeight: 'bold', gap: 8, padding: 15 }}>
-        <div style={{ color: '#ffdf58', fontSize: '2em' }}>ĐỊA CHỈ ĐĂNG KÝ / BOOKING:</div>
-        <div style={{ color: 'white', fontSize: '1.5em' }}>Hotlines: 0944422361 / 0398689831</div>
-        <div style={{ color: 'white', fontSize: '1.5em' }}>
-          <span>Fanpage: </span>
-          <a href='https://facebook.com/coffeeandtea.aroma' style={{ color: 'white' }} target='_blank'>facebook.com/coffeeandtea.aroma</a>
         </div>
-        <div style={{ color: 'white', fontSize: '1.5em' }}>
-          <div>
-            <span>Địa chỉ booking: </span>
-            <a href='https://docs.google.com/forms/d/e/1FAIpQLSd-9AD_FzLN_rQONQftDAtbknLFOAZSDK5HSu3R6tClttw_cQ/viewform' style={{ color: 'white' }} target='_blank'>Bấm vào đây để đăng ký</a>
+        <div className='hotlines_booking_container'>
+            <div className='hotlines_booking_title'>ĐỊA CHỈ ĐĂNG KÝ / BOOKING:</div>
+            <div className='hotlines'>Hotlines: 0944422361 / 0398689831</div>
+            <div className='fanpage'>
+              <span>Fanpage: </span>
+              <a href='https://facebook.com/coffeeandtea.aroma' style={{ color: 'white' }} target='_blank'>facebook.com/coffeeandtea.aroma</a>
+            </div>
+            <div className='booking_address'>
+              <div>
+                <span>Địa chỉ booking: </span>
+                <a href='https://docs.google.com/forms/d/e/1FAIpQLSd-9AD_FzLN_rQONQftDAtbknLFOAZSDK5HSu3R6tClttw_cQ/viewform' style={{ color: 'white' }} target='_blank'>Bấm vào đây để đăng ký</a>
+              </div>
+              <div style={{ marginTop: 8 }}>Hoặc quét mã QR tại đây:</div>
+              <div style={{ paddingTop: 8 }}>
+                <img src={qrcodeImage} alt="QRCode Registration Form" style={{ width: 100, height: 'auto' }} />
+              </div>
+            </div>
           </div>
-          <div style={{ marginTop: 8 }}>Hoặc quét mã QR tại đây:</div>
-          <div style={{ paddingTop: 8 }}>
-            <img src={qrcodeImage} alt="QRCode Registration Form" style={{ width: 100, height: 'auto' }} />
-          </div>
-        </div>
-      </div>
-        </div>
       </div>
     </div>
   );
